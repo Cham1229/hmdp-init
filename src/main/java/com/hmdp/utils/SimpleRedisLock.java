9fc42a403048be1f86b8bf6c727974d1d2ef954b
cham <luhaocheng729@gmail.com>
2025-09-16 23:35:28 +0800

    线程标识解决分布式锁误删问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,9 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import java.util.UUID;
+
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -16,19 +17,31 @@ public class SimpleRedisLock implements ILock{
     }
 
     private static final String KEY_PREFIX = "lock:";
+    //线程标识
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
 
     @Override
     public boolean tryLock(long timeoutSec) {
         // 获取线程id
-        String value = Thread.currentThread().getId() + "";
+        String threadId =ID_PREFIX + Thread.currentThread().getId() + "";
         // 生成锁的key
         String key = KEY_PREFIX + name;
-        Boolean success = stringredistemplate.opsForValue().setIfAbsent(key, value, timeoutSec, TimeUnit.SECONDS);
+        Boolean success = stringredistemplate.opsForValue().setIfAbsent(key, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public void unLock() {
-        stringredistemplate.opsForValue().getAndDelete(KEY_PREFIX + name);
+        // 获取线程id，相当于原来锁的值
+        String threadId = ID_PREFIX + Thread.currentThread().getId() + "";
+        // 获取当前锁的值
+        String key = KEY_PREFIX + name;
+        String id = stringredistemplate.opsForValue().get(key);
+        // 判断锁是否属于当前线程
+        if (threadId.equals(id)){
+            stringredistemplate.opsForValue().getAndDelete(KEY_PREFIX + name);
+        }
+
+
     }
 }
