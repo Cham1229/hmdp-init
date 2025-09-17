3470418fb4547f09d1aa79372d8c710fd68ce728
cham <luhaocheng729@gmail.com>
2025-09-17 20:47:40 +0800

    调用lua脚本改造分布式锁

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,13 @@
 package com.hmdp.utils;
 
+
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -19,6 +23,12 @@ public class SimpleRedisLock implements ILock{
     private static final String KEY_PREFIX = "lock:";
     //线程标识
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(long timeoutSec) {
@@ -32,16 +42,19 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unLock() {
-        // 获取线程id，相当于原来锁的值
-        String threadId = ID_PREFIX + Thread.currentThread().getId() + "";
-        // 获取当前锁的值
-        String key = KEY_PREFIX + name;
-        String id = stringredistemplate.opsForValue().get(key);
-        // 判断锁是否属于当前线程
-        if (threadId.equals(id)){
-            stringredistemplate.opsForValue().getAndDelete(KEY_PREFIX + name);
-        }
-
-
+        //调用lua脚本
+        stringredistemplate.execute(UNLOCK_SCRIPT, Collections.singletonList(KEY_PREFIX + name), ID_PREFIX + Thread.currentThread().getId());
     }
+//    @Override
+//    public void unLock() {
+//        // 获取线程id，相当于原来锁的值
+//        String threadId = ID_PREFIX + Thread.currentThread().getId() + "";
+//        // 获取当前锁的值
+//        String key = KEY_PREFIX + name;
+//        String id = stringredistemplate.opsForValue().get(key);
+//        // 判断锁是否属于当前线程
+//        if (threadId.equals(id)) {
+//            stringredistemplate.opsForValue().getAndDelete(KEY_PREFIX + name);
+//        }
+//    }
 }
